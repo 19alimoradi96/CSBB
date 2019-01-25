055981d0e1aaca03758cc4acd3d120ffe23087c4
Ali Moradi <alimoradi.9332161013@yahoo.com>
2019-01-25 19:19:17 +0330

    create objects 1

---
+++ Circle Swip Brick Breaker/Circle Swip Brick Breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+	private Vector3 mouseDownPos;
+    private Transform ballTransform;
+	public GameObject ball;
+    private Camera cam;
+	// Use this for initialization
+	void Start () {
+        cam = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+            Debug.Log(Input.mousePosition);
+            mouseDownPos = Input.mousePosition;
+        }
+		if (Input.GetMouseButtonUp (0)) {
+            Debug.Log(Input.mousePosition);
+            Vector3 shoot = cam.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log(shoot);
+            ball.GetComponent<Rigidbody>().AddForce(shoot * 100.0f);
+        }
+	}
+}
